a637f96d6a2f55bb32019e9b23436bee22ad8d77
Christopher Tran <chris@CHARLEYTRANs-MacBook-Pro.local>
2026-08-20 17:32:51 -0400

    Add unit tests for GradeAnalyzer.calculateAverage and junit jar

---
+++ module4/MyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;  
+
+public class MyTest {
+    @Test
+    void testSomething() {
+        assertEquals(5, 5);
+    }
+}
